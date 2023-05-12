a3cc26af57319c0ef15e210bb2a9f86c98d6b2d9
Tariq <rafiqtariq19@gmail.com>
2023-05-12 23:23:46 +0530

    Page Redirection fixed in signup

---
+++ app/src/main/java/com/example/crimelocator/LoggedInUserDetails.java
@@ -0,0 +1,14 @@
+package com.example.crimelocator;
+
+public class LoggedInUserDetails {
+
+    String Username = "";
+    String Email = "";
+    String Number = "";
+
+    LoggedInUserDetails(String username, String email, String number){
+        this.Username = username;
+        this.Email = email;
+    }
+
+}
