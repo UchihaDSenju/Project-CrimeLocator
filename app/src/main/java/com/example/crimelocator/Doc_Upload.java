09f12e3b157f3f5c67a9905db17a4f56d66d99cc
AHMEDSAHEEL <shaheel123as@gmail.com>
2023-06-05 15:02:19 +0530

    some useless change

--- app/src/main/java/com/example/crimelocator/Doc_Upload.java
+++ app/src/main/java/com/example/crimelocator/Doc_Upload.java
@@ -49,7 +49,7 @@ public class Doc_Upload extends AppCompatActivity {
 
                    Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT); //ACTION_GET_CONTENT
                    intent.setType("*/*");
-                   startActivityForResult(intent,10);
+                   startActivityForResult(intent,10);  //reqcode
                }
            });
            
