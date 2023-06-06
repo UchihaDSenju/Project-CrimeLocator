3360b7c8dc31e3b6ad7b5e284c845ed4c3744276
AHMEDSAHEEL <shaheel123as@gmail.com>
2023-06-06 21:12:14 +0530

    Gallery view added

---
+++ app/src/main/java/com/example/crimelocator/Gallery_View.java
@@ -0,0 +1,14 @@
+package com.example.crimelocator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Gallery_View extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery_view);
+    }
+}
