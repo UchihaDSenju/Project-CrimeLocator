e1ea97a5446f9ddc54daf177726ea4d716e13f2a
AHMEDSAHEEL <shaheel123as@gmail.com>
2023-06-06 22:47:08 +0530

    Gallery View Designed

---
+++ app/src/main/java/com/example/crimelocator/galleryData.java
@@ -0,0 +1,18 @@
+package com.example.crimelocator;
+
+public class galleryData {
+
+    private Integer galleryImage;
+
+    public galleryData(Integer galleryImage) {
+        this.galleryImage = galleryImage;
+    }
+
+    public Integer getGalleryImage() {
+        return galleryImage;
+    }
+
+    public void setGalleryImage(Integer galleryImage) {
+        this.galleryImage = galleryImage;
+    }
+}
