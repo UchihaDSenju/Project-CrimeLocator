2c5b0c220fe1a5afbd3cd9b361ab4a59b7c581ae
AHMEDSAHEEL <shaheel123as@gmail.com>
2023-06-07 20:42:39 +0530

    Helped User List setted and admin select image from file manager

--- app/src/main/java/com/example/crimelocator/Add_News.java
+++ app/src/main/java/com/example/crimelocator/Add_News.java
@@ -1,8 +1,12 @@
 package com.example.crimelocator;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Add_News extends AppCompatActivity {
 
@@ -11,5 +15,21 @@ public class Add_News extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_news);
         getSupportActionBar().hide();
+
+        Button adminSelectImage =findViewById(R.id.adminSelectImage);
+
+        adminSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT); //ACTION_GET_CONTENT
+                intent.setType("image/*");
+                startActivityForResult(intent,10);  //reqCode
+            }
+        });
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/crimelocator/UserData.java
@@ -0,0 +1,17 @@
+package com.example.crimelocator;
+
+public class UserData {
+    private String userHelpText;
+
+    public UserData(String userHelpText) {
+        this.userHelpText = userHelpText;
+    }
+
+    public String getUserHelpText() {
+        return userHelpText;
+    }
+
+    public void setUserHelpText(String userHelpText) {
+        this.userHelpText = userHelpText;
+    }
+}
